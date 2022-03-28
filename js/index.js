223255397f6a06cb5d8d7ca618a0d29c6ede11e8
Jyrex Ronaldo <jyrexgt38@gmail.com>
2022-03-28 04:33:31 +0000

    Allow many evaluation of numbers with operators

--- js/index.js
+++ js/index.js
@@ -4,50 +4,62 @@ const operators = Array.from(document.querySelectorAll('.operator'));
 const equalSign = document.querySelector('.equalSign');
 const clearButton = document.querySelector('.clear');
 let displayValue = null;
+let operation = null;
 let firstNum = null;
 let secondNum = null;
-let operation = null;
 let calculation = null;
+let checker = 'on';
 
 numbers.forEach((number) => {
     number.addEventListener('click', (e) => {
+        if (checker === 'off') {
+            display.textContent = null;
+            checker = 'on';
+        }
         display.textContent += e.target.textContent;
         displayValue = +display.textContent;
-        console.log(displayValue);
     });
 });
 
 operators.forEach((operator) => {
     operator.addEventListener('click', (e) => {
-        firstNum = displayValue;
+        checker = 'off';
+        if (firstNum) {
+            secondNum = displayValue;
+            console.log(`Second num: ${secondNum}`);
+            if (operation === '/') {
+                calculation = divide;
+            } else if (operation === '*') {
+                calculation = multiply;
+            } else if (operation === '-') {
+                calculation = subtract;
+            } else if (operation === '+') {
+                calculation = add;
+            }
+
+            display.textContent = operate(calculation, firstNum, secondNum);
+            firstNum = +display.textContent;
+            secondNum = null;
+        } else {
+            firstNum = displayValue;
+            console.log(`First num: ${firstNum}`);
+        }
         operation = e.target.textContent;
-        display.textContent = null;
+        
     });
 });
 
 equalSign.addEventListener('click', (e) => {
-    secondNum = +display.textContent
-
-    if (operation === '/') {
-        calculation = divide;
-    } else if (operation === '*') {
-        calculation = multiply;
-    } else if (operation === '-') {
-        calculation = subtract;
-    } else if (operation === '+') {
-        calculation = add;
-    }
-
-    display.textContent = operate(calculation, firstNum, secondNum)
-
+    
 });
 
 clearButton.addEventListener('click', () => {
     displayValue = null;
+    operation = null;
     firstNum = null;
     secondNum = null;
-    operation = null;
     calculation = null;
+    checker = 'on';
     display.textContent = null;
 });
 
@@ -71,4 +83,46 @@ function operate(operator, a, b) {
     return operator(a, b)
 }
 
-console.log(divide);
+/*
+!ST NUMBER 
+Press number 
+present number
+store the number for use in first number container
+
+
+Press operation 
+Store operation value for use
+
+2ND NUMBER
+press number again
+check if first number container is true/full
+if so clear diplay
+present number 
+store the number in second number container
+
+
+
+options available = 2:
+press equal sign
+check operation
+evaluate operation and present it
+OR
+PRESS OPERATION AGAIN
+press new operation 
+evaluates old operation and present it
+store result in first number container
+empty second number container
+*/
+
+
+// if (secondNum) {
+//     if (operation === '/') {
+//         calculation = divide;
+//     } else if (operation === '*') {
+//         calculation = multiply;
+//     } else if (operation === '-') {
+//         calculation = subtract;
+//     } else if (operation === '+') {
+//         calculation = add;
+//     }
+// }
