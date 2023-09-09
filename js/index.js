a8d3d02cf2ab9b683099113322654b2415d36493
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2023-09-09 02:25:14 +0000

    Implement structure and design of calculator

---
+++ js/index.js
@@ -0,0 +1,25 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
+
+console.log(add(4,52))
